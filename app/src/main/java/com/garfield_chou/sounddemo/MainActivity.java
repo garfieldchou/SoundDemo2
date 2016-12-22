75701b46fc97b9b828cfbba056c3220145606626
garfield_chou <garfield_chou@asus.com>
2016-12-22 17:44:43 +0800

    Set SeekBar for volume control, AudioManager, getSystemService(), getStreamMaxVolume(), getStreamVolume(), SeekBar, setMax(), setProgress()

--- app/src/main/java/com/garfield_chou/sounddemo/MainActivity.java
+++ app/src/main/java/com/garfield_chou/sounddemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.garfield_chou.sounddemo;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.widget.SeekBar;
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mplayer;
+    AudioManager audioManager;
 
     public void audioPlay(View view) {
         mplayer.start();
@@ -25,12 +28,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mplayer = MediaPlayer.create(this, R.raw.laugh);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
         SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(curVolume);
+
         volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 Log.i("Seekbar value", Integer.toString(progress));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
             }
 
             @Override
