3268a2d0b12828b033526b01c9d05ca784b9da8c
garfield_chou <garfield_chou@asus.com>
2016-12-22 14:41:02 +0800

    Add SeekBar and its setOnSeekBarChangeListener(), and OnSeekBarChangeListener()

--- app/src/main/java/com/garfield_chou/sounddemo/MainActivity.java
+++ app/src/main/java/com/garfield_chou/sounddemo/MainActivity.java
@@ -3,7 +3,9 @@ package com.garfield_chou.sounddemo;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mplayer = MediaPlayer.create(this, R.raw.laugh);
+
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("Seekbar value", Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
